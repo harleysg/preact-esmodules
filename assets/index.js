b364cf3478957802e46738019d09bedb401122f2
Harley Santos Garzon <harley.santos.garzon@gmail.com>
2020-03-29 23:54:29 -0500

    refactor(index.{ts,js}): se integra componente hakerNews

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,8 @@
 import { html, render } from "./libs/preact.js";
+import HackerNewsApp from "./components/hackerNews.js";
 render(
 	html`
-		HackerNewsApp
+		<${HackerNewsApp} />
 	`,
 	document.body
 );
--- assets/index.ts
+++ assets/index.ts
@@ -1,8 +1,9 @@
 import { html, render } from "./libs/preact.js";
+import HackerNewsApp from "./components/hackerNews.js";
 
 render(
 	html`
-		HackerNewsApp
+		<${HackerNewsApp} />
 	`,
 	document.body
 );
